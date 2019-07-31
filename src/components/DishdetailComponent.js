27b14412940bf787f231928ef958c5e80cef862e
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-07-31 16:35:55 -0500

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 
-class Dishdetail extends Component {
-    constructor(props) {
-        super(props);
+    function RenderDish({dish}) {
+		return(
+            <div className="col-12 col-md-5 m-1">
+				<Card>
+					<CardImg width="100%" src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>	
+				</Card>
+            </div>                   
+		);
     }
 
-    renderDish(dish) {
-		if (dish != null) {
-			return(
-                <div className="col-12 col-md-5 m-1">
-					<Card>
-						<CardImg width="100%" src={dish.image} alt={dish.name} />
-						<CardBody>
-							<CardTitle>{dish.name}</CardTitle>
-							<CardText>{dish.description}</CardText>
-						</CardBody>	
-					</Card>
-                </div>
-                       
-			);
-		}
-		else {
-			return(
-				<div></div>
-			);
-		}
-    }
-
-    renderComments(comments) {
+    function RenderComments({comments}) {
+        
         if (comments == null) 
         { 
             return <div></div>   
@@ -57,21 +45,20 @@ class Dishdetail extends Component {
         )
     }
 
-    render() {
-        if(this.props.dish == null)
+    const Dishdetail = (props) => {
+        
+        if(props.dish == null)
         {
             return(<div></div>)
         }
         return(
          <div className="container">
             <div className="row">
-                {this.renderDish(this.props.dish)}
-                {this.renderComments(this.props.dish.comments)}
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.dish.comments} />
             </div>
          </div>   
                   
         )
     }
-}
-
 export default Dishdetail;
