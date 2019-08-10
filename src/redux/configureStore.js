9b092b9523c1647a5d0301e22c66fe0ee4659855
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-08-10 14:31:18 -0500

    React Redux Form Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
@@ -13,7 +15,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
---
+++ src/redux/forms.js
@@ -0,0 +1,9 @@
+export const InitialFeedback = {
+    firstName: '',
+    lastName: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contactType: 'Tel. ',
+    message: ''
+}
